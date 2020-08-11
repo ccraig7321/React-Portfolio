2615b2abac7d0c4748a13d71acf9d32a1ae4a4e4
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-08-10 21:34:45 -0400

    Updates

---
+++ precache-manifest.7e520372b3b58992fb953f4ac7a7a321.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c81ec2541c5c6f5d1e1779882510789",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "8ef768ba04ecb59c86c6",
+    "url": "/React-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2d017f245b979218be5f",
+    "url": "/React-Portfolio/static/css/main.21ab95a1.chunk.css"
+  },
+  {
+    "revision": "8ef768ba04ecb59c86c6",
+    "url": "/React-Portfolio/static/js/2.daff0aed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.daff0aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d017f245b979218be5f",
+    "url": "/React-Portfolio/static/js/main.892ba969.chunk.js"
+  },
+  {
+    "revision": "0274f599536680666069",
+    "url": "/React-Portfolio/static/js/runtime-main.27e0f504.js"
+  },
+  {
+    "revision": "d6d1817e2815d64e5415bd114dc4dcb7",
+    "url": "/React-Portfolio/static/media/Headshot2014.d6d1817e.jpg"
+  },
+  {
+    "revision": "1785839ec274d8eea8f85c43eafe6e92",
+    "url": "/React-Portfolio/static/media/budgetHomepage.1785839e.png"
+  },
+  {
+    "revision": "0191010579bd2ea1742cf251a638c32d",
+    "url": "/React-Portfolio/static/media/burgerPicture.01910105.png"
+  },
+  {
+    "revision": "5c913c21605f9e9cb85210e49b973bd3",
+    "url": "/React-Portfolio/static/media/combat.5c913c21.png"
+  },
+  {
+    "revision": "c614347ab1c7c0ae96b267c57f666aef",
+    "url": "/React-Portfolio/static/media/full-bloom.c614347a.png"
+  },
+  {
+    "revision": "535f8835253765c69519c70695905cc2",
+    "url": "/React-Portfolio/static/media/noteTakerHomepage.535f8835.png"
+  },
+  {
+    "revision": "2bab3500d20ac9203b09e9cb9a15c4ef",
+    "url": "/React-Portfolio/static/media/plasma.2bab3500.png"
+  },
+  {
+    "revision": "b5061e3fe2650638b8dd2320b93e14cd",
+    "url": "/React-Portfolio/static/media/stats.b5061e3f.png"
+  },
+  {
+    "revision": "264b5c48bcc16b211f8cc8ea6aaa095f",
+    "url": "/React-Portfolio/static/media/teamBuilder.264b5c48.png"
+  },
+  {
+    "revision": "c62b41df3703421fe4ab2248f2faff27",
+    "url": "/React-Portfolio/static/media/userDirectory.c62b41df.png"
+  }
+]);
